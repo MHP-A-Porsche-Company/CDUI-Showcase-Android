0c4689b126034bfddb37f5316db80fbf60680754
Benjamin Vogt <benjamin.vogt@mhp.com>
2017-10-23 12:08:37 +0200

    Deleted obsolete code block

--- app/src/main/kotlin/com/mhp/showcase/block/articlestream/ArticleStreamBlock.kt
+++ app/src/main/kotlin/com/mhp/showcase/block/articlestream/ArticleStreamBlock.kt
@@ -14,18 +14,4 @@ data class ArticleStreamBlock(
         val created: Long,
         val target: URI
 
-) : BaseBlock {
-//    override fun equals(other: Any?): Boolean {
-//        return super.equals(other)
-//    }
-//
-//    override fun hashCode(): Int {
-//        var result = id.hashCode()
-//        result = 31 * result + user.hashCode()
-//        result = 31 * result + imageUrl.hashCode()
-//        result = 31 * result + title.hashCode()
-//        result = 31 * result + subtitle.hashCode()
-//        result = 31 * result + created.hashCode()
-//        return result
-//    }
-}
+) : BaseBlock
